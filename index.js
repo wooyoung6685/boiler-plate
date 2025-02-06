b06413c6ee37756b7b243a3f8ee8c88f0b7ee6ad
최우영 <choiwooyoung@choeuyeong-ui-MacBookPro.local>
2025-02-06 12:41:24 +0900

    env설정

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const app = express();
 const port = 5001;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const config = require("./config/key");
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://wooyoung:chldndud1!@boilerplate.thpdd.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("Mongoose Connect"))
   .catch((err) => console.log("Mongoose Error:", err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => res.status(200).json({ success: true }))
+    .catch((err) => res.status(400).json({ success: false, err }));
 });
 
 app.listen(port, () => {
