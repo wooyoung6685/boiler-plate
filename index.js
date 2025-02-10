a9fff54fd92b8cfedc20a22656b4f511ef1fe672
최우영 <choiwooyoung@choeuyeong-ui-MacBookPro.local>
2025-02-10 10:37:31 +0900

    토큰설정

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5001;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const config = require("./config/key");
 
@@ -28,6 +30,42 @@ app.post("/register", (req, res) => {
     .catch((err) => res.status(400).json({ success: false, err }));
 });
 
+app.post("/login", (req, res) => {
+  // 이메일이 DB에 있는지 확인
+  User.findOne({
+    email: req.body.email,
+  })
+    .then(async (user) => {
+      if (!user) {
+        throw new Error("제공된 이메일에 해당하는 유저가 없습니다.");
+      }
+      // 비밀번호가 일치하는지 확인
+      const isMatch = await user.comparePassword(req.body.password);
+      return { isMatch, user };
+    })
+    .then(({ isMatch, user }) => {
+      console.log(isMatch);
+      if (!isMatch) {
+        throw new Error("비밀번호가 틀렸습니다.");
+      }
+      // 로그인 완료
+      return user.generateToken();
+    })
+    .then((user) => {
+      // 토큰 저장 (쿠키, localstorage ...)
+      return res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+        userId: user._id,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({
+        loginSuccess: false,
+        message: err.message,
+      });
+    });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -31,6 +34,43 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.pre("save", function (next) {
+  // 비밀번호 암호화
+  const user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        return next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword) {
+  // 암호화된 비밀번호와 같은지 체크
+  const user = this;
+  return bcrypt.compare(plainPassword, this.password);
+};
+
+userSchema.methods.generateToken = function () {
+  // jwt 생성
+  user = this;
+  const token = jwt.sign(user._id.toJSON(), "secretToken");
+  user.token = token;
+
+  return user.save();
+};
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
